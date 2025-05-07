f42474facb9386795f4fe90041819bd9d7b7e7f9
Dylan <dylannguyen131@gmail.com>
2025-05-07 21:50:34 +1000

    Remove Cors

--- backend/main.py
+++ backend/main.py
@@ -20,13 +20,7 @@ app.include_router(metric.router)
 app.include_router(run.router)
 app.include_router(ws.router)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# Add CORS middleware here
 
 @app.post("/delete-data")
 async def delete_data():
